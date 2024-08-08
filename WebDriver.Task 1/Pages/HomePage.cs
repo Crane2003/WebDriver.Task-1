79abbebdbcd85d5e548724ae525e560525ddd436
Crane2003 <y.v.miasnykov@student.khai.edu>
2024-08-08 10:39:35 +0300

    Add project files.

---
+++ WebDriver.Task 1/Pages/HomePage.cs
@@ -0,0 +1,46 @@
+﻿using OpenQA.Selenium;
+
+namespace WebDriver.Task_1.Pages
+{
+    public class HomePage
+    {
+        private readonly IWebDriver driver;
+        private const string url = "https://pastebin.com/";
+
+        public HomePage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        private IWebElement NewPasteTextArea => driver.FindElement(By.Id("postform-text"));
+        private IWebElement PasteNameInput => driver.FindElement(By.Id("postform-name"));
+        private IWebElement CreateNewPasteButton => driver.FindElement(By.XPath("//button[text()='Create New Paste']"));
+
+        public void Open()
+        {
+            driver.Navigate().GoToUrl(url);
+        }
+
+        public void EnterPasteText(string pasteText)
+        {
+            NewPasteTextArea.SendKeys(pasteText);
+        }
+
+        public void SelectPasteExpiration(string expiration)
+        {
+
+            IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+            js.ExecuteScript($"document.getElementById('postform-expiration').value = '{expiration}';");
+        }
+
+        public void EnterPasteName(string pasteName)
+        {
+            PasteNameInput.SendKeys(pasteName);
+        }
+
+        public void CreateNewPaste()
+        {
+            CreateNewPasteButton.Click();
+        }
+    }
+}
---
+++ WebDriver.Task 1/Tests/PastebinTests.cs
@@ -0,0 +1,46 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using WebDriver.Task_1.Pages;
+
+namespace WebDriver.Task_1.Tests
+{
+    [TestClass]
+    public class PastebinTests
+    {
+        private IWebDriver driver;
+        private HomePage homePage;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            homePage = new HomePage(driver);
+        }
+
+        [TestMethod]
+        public void CreateNewPaste_Test()
+        {
+            // Arrange
+            string pasteText = "Hello from WebDriver";
+            string pasteExpiration = "10M";
+            string pasteName = "helloweb";
+
+            // Act
+            homePage.Open();
+            homePage.EnterPasteText(pasteText);
+            homePage.SelectPasteExpiration(pasteExpiration);
+            homePage.EnterPasteName(pasteName);
+            homePage.CreateNewPaste();
+
+            // Assert
+            Assert.IsTrue(driver.PageSource.Contains(pasteText));
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+    }
+}
